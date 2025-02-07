2243c67251a55cd764454e335887134de4d19240
CarlosValArg <cavalar1999@gmail.com>
2025-02-07 12:26:20 -0600

    Se añaden funcionalidades de inversión

--- src/app/services/investment.service.ts
+++ src/app/services/investment.service.ts
@@ -9,45 +9,38 @@ import { LocalStorageService } from 'ngx-webstorage';
 })
 export class InvestmentService {
 
-  private investmentIdCounter = 1; // Variable para llevar la cuenta de las inversiones
+  private investmentIdCounter = 1;
 
   constructor(private storage: LocalStorageService) { }
 
-  // Agregar inversión a un cliente
   public addInvestment(customer: ICustomer, amount: number, investRate: number): Observable<IInvest> {
     return new Observable(observer => {
-      // Verificar si el monto de la inversión es mayor que 0
       if (amount <= 0) {
         observer.error('Amount must be greater than zero');
         return;
       }
 
-      // Crear una nueva inversión con los parámetros recibidos
       const newInvestment: IInvest = {
-        investmentId: `INV-${this.investmentIdCounter}`, // Usamos un ID fijo para la inversión
-        balance: amount,  // Asignamos el balance de la inversión
-        transactions: [  // Aquí creamos la transacción vinculada a la inversión
+        investmentId: `INV-${this.investmentIdCounter}`, 
+        balance: amount,  
+        transactions: [  
           {
-            transactionId: `TX-${this.investmentIdCounter}`,  // Transacción vinculada
-            accountId: `INV-${this.investmentIdCounter}`, // ID único para la cuenta de inversión
-            amount: amount,  // El monto de la inversión
-            transactionDate: new Date(),  // Fecha actual de la transacción
-            transactionType: TranscationType.DEPOSIT  // Tipo de transacción: Depósito
+            transactionId: `TX-${this.investmentIdCounter}`,  
+            accountId: `INV-${this.investmentIdCounter}`, 
+            amount: amount, 
+            transactionDate: new Date(),  
+            transactionType: TranscationType.DEPOSIT
           }
         ],
-        investRate: investRate  // Asignamos el tasa de interés (investRate)
+        investRate: investRate
       };
 
-      // Agregar la nueva inversión a las inversiones del cliente
       customer.investments.push(newInvestment);
 
-      // Incrementamos el contador para el próximo ID
       this.investmentIdCounter++;
 
-      // Guardar el cliente con la nueva inversión
       this.storage.store('customer', customer);
 
-      // Emitir el nuevo objeto de inversión a los suscriptores
       observer.next(newInvestment);
       observer.complete();
     });
